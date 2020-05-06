4ef7d70111a3f7230ab1adea46e72021491a7cbf
demartinoaj <demartinoaj@gmail.com>
2020-05-06 12:40:02 -0400

    Updating python script with new state machine

--- UART.py
+++ UART.py
@@ -1,6 +1,10 @@
 import serial
 
-s = serial.Serial('COM13')
-while(1):
-    res = s.read()
-    print((res.decode('UTF-8')))
+s = serial.Serial('COM9')
+f = open('output.txt','w')
+print("Dumping SRAM contents")
+s.write(b'a')
+for i in range(0,0x1FFF):
+    val = s.read().hex()
+    f.write(val)
+    f.write("\n")
