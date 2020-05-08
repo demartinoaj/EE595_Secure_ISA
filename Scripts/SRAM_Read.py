965a7c4fc04c6acd05a5ebca14bcc46aa6d2069e
demartinoaj <demartinoaj@gmail.com>
2020-05-08 11:21:59 -0400

    Reorganizing git repo

---
+++ Scripts/SRAM_Read.py
@@ -0,0 +1,36 @@
+import serial
+import os
+import sys
+
+
+dirpath = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dirpath)
+
+filepath1 = os.path.join(dirpath,'SRAM_Data10.txt')
+
+
+file1 = open(filepath1,"w")
+
+s = serial.Serial('COM13')
+KeyByte=b''
+J=0
+i=0
+SRAM_Address=0x0000
+while(1):
+    res = s.read()
+    KeyByte+=res
+    J=J+1
+    if( J==5):
+        print(KeyByte)
+        file1.write(str(KeyByte)+"\t"+str(hex(SRAM_Address))+"\n")
+        file1.flush()
+        KeyByte=b''
+        i+=1
+        J=0
+        SRAM_Address+=1
+
+    if(i==8192):
+         break
+
+print(KeyByte)
+file1.close()
